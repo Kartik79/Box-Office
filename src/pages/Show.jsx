f4717b992971c77f39d00233125dc789e2ab90cf
Kartik Tiwari <tiwarikartik79@gmail.com>
2023-07-08 20:28:59 +0530

    using custom hooks to fetch show data

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getShowbyId } from "../api/tvmaze";
+
+const useShowById=(showId)=> {
+    const {showdata,setshowdata} = useState(null)
+    const {showerror, setshowerror} = useState(null)
+
+    useEffect(()=>{
+        async function FetchData() {
+            try {
+                const data=await getShowbyId(showId)
+                setshowdata(data)
+            } catch(err) {
+                setshowerror(err)
+            }
+        }
+        FetchData()
+    },[showId])
+    return {showdata,showerror}
+}
 
 const Show=()=>{
     const {showId}=useParams()
-    return <div>Show Page for {showId}</div>
+    const {showdata,showerror} = useShowById(showId)
+
+    if(showerror) {
+        return <div>We have an error : {showerror.message}</div>
+    }
+    if(showdata) {
+        return <div>Got show data : {showdata.name}</div>
+    }
+    return <div>Page loading</div>
 }
 export default Show
